9bc790ff6e11d05303e8ea8fc8e7a00f1e2c7583
Ryan <dawkinssu@gmail.com>
2018-02-22 23:24:26 -0600

    Adding additional text.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 This piece of middleware is to simulate that a real person is typing between messages that are sent from the bot to the user. This is done by sending a typing indicator between messages. 
 The delay is determined by the number of words in the MessageActivity.Text field and the words per minute configured from the middlewares constructor.
 
+The purpose of this middleware is to give a more realistic feel to the bot as it simulates a person. Another reason is to prevent the user from having to scroll up to read a number of
+messages that may have came in from the bot all at once.
+
 # Usage
 This piece of middleware is intended for the latest version of the BotFramework which can be built from the [v4 github repository](http://github.com/microsoft/botbuilder-dotnet). 
 
---
+++ Typist/TypistSample/Controllers/MessagesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Storage;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using RyanDawkins.Typist.Middleware;
+using static Microsoft.Bot.Builder.Middleware.MiddlewareSet;
+
+namespace TypistSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        private BotFrameworkAdapter _adapter;
+
+        /// <summary>
+        /// In this sample Bot, a new instance of the Bot is created by the controller 
+        /// on every incoming HTTP reques. The bot is constructed using the credentials
+        /// found in the config file. Note that no credentials are needed if testing
+        /// the bot locally using the emulator. 
+        /// </summary>        
+        public MessagesController(IConfiguration configuration)
+        {
+            var bot = new Bot(new BotFrameworkAdapter(configuration))
+                .Use(new TypistMiddleware(90));
+            bot.OnReceive(BotReceiveHandler);
+
+            _adapter = (BotFrameworkAdapter)bot.Adapter;
+        }
+
+        private async Task BotReceiveHandler(IBotContext context, NextDelegate next)
+        {
+            if (context.Request.Type == ActivityTypes.Message)
+            {
+                context.Reply(context.Request.AsMessageActivity().Text);
+            }
+
+            await next();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Activity activity)
+        {
+            try
+            {
+                await _adapter.Receive(this.Request.Headers["Authorization"].FirstOrDefault(), activity);
+                return this.Ok();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return this.Unauthorized();
+            }
+        }
+    }
+}
